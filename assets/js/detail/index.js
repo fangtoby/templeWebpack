43a9d01b3021699ff18c31625ee4bf8fdf56d2bf
fangtoby <fangtoby@live.cn>
2016-07-16 23:52:24 +0800

    update

--- assets/js/detail/index.js
+++ assets/js/detail/index.js
@@ -1,1 +1,17 @@
-webpackJsonp([3],[function(l,o,n){(function(l){l("body").addClass("shelloworld")}).call(o,n(1))}]);
+webpackJsonp([3],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function($) {
+	if(true){
+
+		console.log('is detail!');
+		console.log('works.');
+		
+	}
+
+	$('body').addClass('shelloworld');
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }
+]);
