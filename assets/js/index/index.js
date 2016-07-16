bd61a8792807a5ed50110b78add1697394b04ff8
fangtoby <fangtoby@live.cn>
2016-07-16 23:52:24 +0800

    update

--- assets/js/index/index.js
+++ assets/js/index/index.js
@@ -1,1 +1,41 @@
-webpackJsonp([2],[function(o,l,n){(function(o){var l=null;o(".show_pop").click(function(o){n.e(0,function(){var o=n(6);null==l&&(l=new o),l.show({autoClose:!0,timeout:4e3})})}),o(".close_pop").click(function(o){l.hide()}),o("body").addClass("helloworld")}).call(l,n(1))}]);
+webpackJsonp([2],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function($) {
+
+
+	if(true){
+
+		console.log('is developer!');
+		console.log('works.');
+
+	}
+
+	var p =  null;
+
+	$('.show_pop').click(function (argument) {
+
+		__webpack_require__.e/* nsure */(0, function() { // 语法奇葩, 但是有用
+			var pop = __webpack_require__(6);
+
+			if(p == null){
+				p = new pop();
+			}
+			p.show({
+				autoClose: true,
+				timeout: 4000
+			});
+		});
+	});
+
+	$('.close_pop').click(function (e) {
+		// body...
+		p.hide();
+	});
+
+	$('body').addClass('helloworld');
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }
+]);
